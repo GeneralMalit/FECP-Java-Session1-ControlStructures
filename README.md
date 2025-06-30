332959fd7a36811329a8f8cb11a78988ed644395
General Malit <general.malit@gfitftlapt04013.ad.mynt.xyz>
2025-06-30 15:34:45 +0800

    Solution with 5/5 passed test cases

---
+++ README.md
@@ -0,0 +1,5 @@
+# FECP5-1005-Lab02
+
+### Membership Calculator
+
+![result.png](result.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,51 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        String memberStatus = sc.nextLine();
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        double price;
+        switch(memberStatus){
+            case "Regular":
+                if(age < 18){
+                    price = 50;
+                }else if(age < 65){
+                    price = 100;
+                }else{
+                    price = 75;
+                }
+                System.out.printf("Price: $%.2f", price);
+                break;
+            case "VIP":
+                if(age < 18){
+                    price = 75;
+                }else if(age < 65){
+                    price = 150;
+                }else{
+                    price = 112.5;
+                }
+                System.out.printf("Price: $%.2f", price);
+                break;
+            case "Premium":
+                if(age < 18){
+                    price = 100;
+                }else if(age < 65){
+                    price = 200;
+                }else{
+                    price = 150;
+                }
+                System.out.printf("Price: $%.2f", price);
+
+                break;
+            default:
+                System.out.println("Invalid membership status entered.");
+                break;
+
+        }
+
 
     }
 }
